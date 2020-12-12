f59efb9e10a9fd89f37c1442eb018022a3746ab2
kanjani8 <42990328+kanjani8@users.noreply.github.com>
2020-12-12 18:29:15 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,39 +1,35 @@
 var db = require('./db.js');
-var http = require('http');
+var http = require('http'); // 서버 구동을 위함
+var fs = require('fs'); // 파일 읽어오고 쓰기를 위함
 
 var port = 80;
 
-var app = http.createServer(function(req, res){
-
-    db.query('SELECT * FROM recycleb', function(error, result){
-      console.log(result)
-
-      var html =
-      ` <!doctype html>
-      <html>
-      <head>
-      <title>nodejs - mysql </title>
-      <meta charset="utf-8">
-      </head>
-      <body> nodejs - mysql
-
-      <p>${result[0].id} / ${result[0].address} / ${result[0].grp}</p>
-      <p>${result[1].id} / ${result[1].address} / ${result[1].grp}</p>
-      <p>${result[2].id} / ${result[2].address} / ${result[2].grp}</p>
-      <p>${result[3].id} / ${result[3].address} / ${result[3].grp}</p>
-      <p>${result[4].id} / ${result[4].address} / ${result[4].grp}</p>
-      <p>${result[5].id} / ${result[5].address} / ${result[5].grp}</p>
-      <p>${result[6].id} / ${result[6].address} / ${result[6].grp}</p>
-
-      </body>
-      </html> `
+//404: 페이지 오류가 발생하였을 때
+function send404Message(response){
+  response.writeHead(404,{"Content-Type":"text/plain"});
+  response.write("404 에러");
+  response.end();
+}
 
 
-      res.writeHead(200);
-      res.end(html);
+//200: 정상적인 요청일 때
+function onRequest(request, response){
+  if(request.method =="GET" && request.url == '/'){
+          response.writeHead(200,{"Content-Type":"text/html"});
+          fs.createReadStream("./map.html").pipe(response);
 
-    });
+  }else if (request.method == "GET" && request.url == '/index.css') {
+    response.writeHead(200,{"Content-Type":"text/css"});
+    fs.createReadStream("./index.css").pipe(response);
+    //  res.end(html);
+  }else{
+    //html파일이 없을 경우
+    send404Message(response);
+  }
+}
 
-}).listen(port, "0.0.0.0");
+
+
+ http.createServer(onRequest).listen(port, "0.0.0.0");
 
 console.log('Server running at http://0.0.0.0:'+port);
